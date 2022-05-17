483c46d8c764e7f95075dca08625e5d863d0dcb8
Braylan Dodson <braylan1005@gmail.com>
2022-05-17 15:57:35 -0400

    commit tree c0125f5493a7da99319e6a40eb2481309c5781fb

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(nameListArray){
+    let tempArray = nameListArray;
+    let resultArray = [tempArray[0],tempArray[1]];
+    
+    return resultArray;
+}
+const returnLastTwoDrivers = function(nameListArray= []){
+    let tempArray = nameListArray;
+    let resultArray = [];
+
+    resultArray.push(tempArray[tempArray.length-2]);
+    resultArray.push(tempArray[tempArray.length-1]);
+
+    return resultArray;
+}
+let selectingDrivers = [returnFirstTwoDrivers(),returnLastTwoDrivers()];
+
+function createFareMultiplier(multiplierValue){
+    return function(fare){
+        return fare*multiplierValue;
+    }
+}
+function fareDoubler(fare){
+    return fare*2;
+}
+function fareTripler(fare){
+    return fare*3;
+}
+function selectDifferentDrivers(arrayOfDrivers, chosenFunction){
+chosenFunction(arrayOfDrivers);
+}
